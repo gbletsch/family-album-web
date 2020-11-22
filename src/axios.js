327ff07aca463e9723a4bd102e063aab91ea4db1
Guilherme <gbletsch@gmail.com>
2020-11-22 10:14:40 -0300

    .

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,7 @@ import { TOKEN_KEY } from "./utils/constants";
 
 const token = localStorage.getItem(TOKEN_KEY);
 const instance = axios.create({
-  baseURL: "https://family-album-server.herokuapp.com/",
+  baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
     authorization: `Bearer ${token}`,
   },
